7313639cc7f81f4a9ffee1a45b052537725dba13
Bakary923 <Badiaga.bakary13@gmail.com>
2026-02-05 01:35:04 +0100

    update test config js du au ingress v1

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // config.js
 export const API_BASE_URL = window._env_?.REACT_APP_API_URL 
                             || process.env.REACT_APP_API_URL 
-                            || "http://127.0.0.1/api"; // L'adresse de l'Ingress par défaut
+                            || "http://petite-maison.local/api"; // L'adresse de l'Ingress par défaut
