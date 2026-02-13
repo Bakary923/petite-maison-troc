db4fea2d21cde682afec0e2a00addfc7e8f4140b
Bakary923 <Badiaga.bakary13@gmail.com>
2026-02-13 17:10:32 +0100

    update mock front

--- frontend/src/__mocks__/react-router-dom.js
+++ frontend/src/__mocks__/react-router-dom.js
@@ -1,4 +1,7 @@
+const React = require("react");
+
 module.exports = {
   useNavigate: () => jest.fn(),
-  MemoryRouter: ({ children }) => <div>{children}</div>,
+  MemoryRouter: ({ children }) => React.createElement("div", null, children),
+  Link: ({ children }) => React.createElement("a", null, children),
 };
