3fb9f460c99294d6752ca6760e29e0ac8fa9f60e
Bakary923 <Badiaga.bakary13@gmail.com>
2026-02-13 17:23:59 +0100

    update mock front

--- frontend/src/__mocks__/react-router-dom.js
+++ frontend/src/__mocks__/react-router-dom.js
@@ -1,6 +1,8 @@
-const mockAuthFetch = jest.fn(() =>
-  Promise.resolve({
-    ok: true,
-    json: () => Promise.resolve({ message: "ok" })
-  })
-);
+const React = require("react");
+
+module.exports = {
+  __esModule: true, // <-- ESSENTIEL pour les imports nommés
+  useNavigate: () => jest.fn(),
+  MemoryRouter: ({ children }) => React.createElement("div", null, children),
+  Link: ({ children }) => React.createElement("a", null, children),
+};
