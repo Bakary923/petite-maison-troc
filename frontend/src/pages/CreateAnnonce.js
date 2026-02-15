d3edf4d88c98352f0e356e614ff8dc5ff3de2bd6
Bakary923 <Badiaga.bakary13@gmail.com>
2026-02-15 23:32:22 +0100

    test déploiment

--- frontend/src/pages/CreateAnnonce.js
+++ frontend/src/pages/CreateAnnonce.js
@@ -20,7 +20,7 @@ export default function CreateAnnonce() {
   async function uploadToCloudinary(file) {
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "annonces_secure"); // Ton preset Cloudinary
+    formData.append("upload_preset", "annonces_secure"); // preset Cloudinary
 
     const response = await fetch(
       "https://api.cloudinary.com/v1_1/dnvjmw8tk/image/upload",
